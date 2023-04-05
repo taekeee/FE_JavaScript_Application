445149dbd7b25ced897f6eb2abf03aa2fdc3755a
taek <yeontaek94@naver.com>
2023-04-05 20:45:57 +0900

    solved

---
+++ Problem4.js
@@ -0,0 +1,13 @@
+// 4번 문제.
+
+let user = {
+  Name: "지환",
+  years: 23,
+};
+
+// 아래에 코드를 작성해주세요 ~
+let  {Name, years: age, isAdmin = false} = user;
+
+alert(Name); // "지환"
+alert(age); // 23
+alert(isAdmin); // false
---
+++ Problem5.js
@@ -0,0 +1,9 @@
+// 5번 문제.
+
+let user = {
+  name: "윤정",
+  age: 20,
+};
+
+let json = JSON.stringify(user);
+let user1 = JSON.parse(json);
